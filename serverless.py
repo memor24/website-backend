3580b0777faf36cb4d76a117f34507eb9a1e9a03
MehdiM <myemail@example.com>
2024-05-08 13:53:07 -0600

    initial commit

---
+++ serverless.py
@@ -0,0 +1,23 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')  #define ddb client
+table = dynamodb.Table('views_counter')  #select a ddb table
+
+def lambda_handler(event, context):  
+    #get Key
+    mydata = table.get_item(Key={
+        'id': 'views'
+    })
+
+    #update the Item
+    counter = mydata ['Item']['counter']
+    counter = counter + 1
+
+    #put updated Item into ddb
+    mydata = table.put_item(Item={
+        'id': 'views',
+        'counter': counter
+    })
+
+    return counter
+
